349b1fe6c105e95de42cfd025e5d3e86e317876d
KamranRizvi265 <kamranrizvicadet@gmail.com>
2026-08-06 19:37:14 +0530

    Added Embedding Pipeline

--- 3-rag/app.py
+++ 3-rag/app.py
@@ -1,9 +1,12 @@
 from src.data_loader import load_documents
-
+from src.embedding import EmbeddingPipeline
 
 # Example usage
 
 if __name__ == "__main__":
     data_path = "data"  # Replace with your actual data path
     documents = load_documents(data_path)
+    chunks = EmbeddingPipeline().chunk_documents(documents)
+    vectors = EmbeddingPipeline().embed_chunks(chunks)
+
     print(f"[INFO] Total documents loaded: {len(documents)}")
